c6e6beeb2213bdd4c83739036d138be89750a93e
andyfernandes <andfernandes24@gmail.com>
2018-09-07 20:46:04 -0300

    1-b

---
+++ Lab1Fork/1-a.cpp
@@ -0,0 +1,44 @@
+#include<errno.h>
+#include<signal.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include <math.h>
+#include<iostream>
+#include<sys/wait.h>
+using namespace std;
+
+double calcFibonnaci(int n){	
+	return (1.0/sqrt(5.0)) * pow(((1.0 + sqrt(5.0))/2.0), n) - (1.0/sqrt(5.0)) * pow(((1.0 - sqrt(5.0))/2.0), n);
+}
+
+void imprimirSequencia(int inicio, int fim){
+	for(int i = inicio -1; i < fim; i++){
+		cout << calcFibonnaci(i) << " ";
+	}
+}
+
+int main(){
+	int pid, status;
+	int inicio, fim;
+	
+	cout << "\nPosição Inicial: ";
+	cin >> inicio;
+
+	cout << "\nPosição Final: ";
+	cin >> fim;
+
+	while (inicio < 0){
+		cout << "\nNumero inválido. Digite novamente a posição Inicial: ";
+		cin >> inicio;
+	}
+
+	while (fim < 0){
+		cout << "\nNumero inválido. Digite novamente a posição Final: ";
+		cin >> fim;
+	}
+	
+	imprimirSequencia(inicio, fim);
+
+	exit(0);
+}
---
+++ Lab1Fork/1-b.cpp
@@ -0,0 +1,58 @@
+#include<errno.h>
+#include<signal.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include <math.h>
+#include<iostream>
+#include<sys/wait.h>
+using namespace std;
+
+double calcFibonnaci(int n){	
+	return (1.0/sqrt(5.0)) * pow(((1.0 + sqrt(5.0))/2.0), n) - (1.0/sqrt(5.0)) * pow(((1.0 - sqrt(5.0))/2.0), n);
+}
+
+void imprimirSequencia(int inicio, int fim){
+	for(int i = inicio -1; i < fim; i++){
+		cout << calcFibonnaci(i) << " ";
+	}
+}
+
+int main(){
+	int pid, status;
+	int inicio, fim;
+	
+	cout << "\nPosição Inicial: ";
+	cin >> inicio;
+
+	cout << "\nPosição Final: ";
+	cin >> fim;
+
+	while (inicio < 0){
+		cout << "\nNumero inválido. Digite novamente a posição Inicial: ";
+		cin >> inicio;
+	}
+
+	while (fim < 0){
+		cout << "\nNumero inválido. Digite novamente a posição Final: ";
+		cin >> fim;
+	}
+
+	int meio = (fim + inicio)/2;
+	pid = vfork();
+	pid = vfork();
+
+	if(pid < 0){
+		perror("\nFalha ao fazer o fork.");
+	} else if (pid == 0){
+		//cout << "\nEu sou o processo " << getpid() << ", filho do processo " << getppid() << ". Fiquei encarregado de imprimir essa parte da Sequência de Fibonnaci"<< endl;
+		imprimirSequencia(inicio, meio);
+		inicio = meio + 1;
+		meio = fim;
+	} else {
+		//cout << "\nOlá, eu sou o pai " << getpid() << ", o meu filho é " << pid << endl;
+		wait(&status);
+	}
+
+	exit(0);
+}
