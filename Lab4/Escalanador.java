4feb962a5a0324fe41a9f3b4b0be6cbcecdd5825
andyfernandes <andfernandes24@gmail.com>
2018-10-08 15:41:44 -0300

    Priopridade por tamanho não preemptivo

---
+++ Lab4/Escalanador.java
@@ -0,0 +1,58 @@
+// <Tempo de chegada>, <ID do Processo>, <Burst Time>, <Prioridade>
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Escalanador{
+	private static final String VIRGULA = ",";
+	ArrayList<String> processos;
+
+	Escalanador(){
+		processos = new ArrayList();
+	}
+
+	public void lerArquivo(String arquivo) throws Exception{
+		BufferedReader readeer = new BufferedReader(new InputStreamReader(new FileInputStream(arquivo)));
+    	String linha = null;
+    	while ((linha = readeer.readLine()) != null) {
+            this.processos.add(linha);
+        }
+	}
+
+	public void imprimir(){
+		for(int i = 0; i < this.processos.size(); i++){
+			System.out.println(this.processos.get(i));
+		}
+	}
+
+	public  void SJFSNaoPreemptivo(){	
+		// processosOrdenados.add(this.processos.get(0));
+		// Levar em consideração o tempo de chegada e o burst time
+		// DEIXAR ISSO AQUI MAIS BONITINHO e realmente testar se não vai ter conflito nessas duas condições do tempo de chegada com o burst time
+		for(int i = 0; i < this.processos.size(); i++){
+			for(int j = 1; j < this.processos.size(); j++){
+				if(Float.parseFloat(this.processos.get(i).split(VIRGULA)[0]) <= Float.parseFloat(this.processos.get(j).split(VIRGULA)[0])){
+					if(Float.parseFloat(this.processos.get(i).split(VIRGULA)[2]) <= Float.parseFloat(this.processos.get(j).split(VIRGULA)[2])){
+						String aux = this.processos.get(j);
+						this.processos.set(j, this.processos.get(i));
+						this.processos.set(i, aux);
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		Escalanador escalonador = new Escalanador();  
+		escalonador.lerArquivo("dados.csv");
+        //String[] dados = escalonador.processos.get(0).split(VIRGULA);
+        //System.out.println("Tempo Chegada: " + dados[0] + "| id: " + dados[1] + " | burst time: " + dados[2] + " | prioridade: " + dados[3] + "\n");
+    	escalonador.SJFSNaoPreemptivo();
+    	escalonador.imprimir();
+    }
+}
