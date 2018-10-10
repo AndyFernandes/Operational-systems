a2a1fb38f463d03e6a4bb8f83bfa932dfaac7087
andyfernandes <andfernandes24@gmail.com>
2018-10-10 12:05:49 -0300

    Lab5

---
+++ Lab5/Dropbox.java
@@ -0,0 +1,40 @@
+public class Dropbox {
+ private int number;
+ private boolean evenNumber = false;
+public boolean available = true;
+public boolean producerPodeProduzir = true;
+
+ public synchronized int take(final boolean even) {
+ 	if(available == true  && checkNumber()==even){
+	 System.out.format("%s CONSUMIDOR obtem %d.%n", even ? "PAR" : "IMPAR",
+	number);
+	 alterarDisponibilidade(false);
+	 producerPodeProduzir = true;
+	}else{
+		
+	}
+
+	return number;
+ }
+
+ public boolean checkNumber(){
+ 	if(this.number % 2 == 0){
+ 		return true;
+ 	}
+ 	return false;
+ }
+
+ public synchronized void alterarDisponibilidade(boolean x){
+ 	available = x;
+ }
+
+ public synchronized void put(int number) {
+ 	if(producerPodeProduzir == true){
+	 this.number = number;
+	 evenNumber = number % 2 == 0;
+	 System.out.format("PRODUTOR gera %d.%n", number);
+	 alterarDisponibilidade(true);
+	 producerPodeProduzir = false;
+	}
+ }
+}
