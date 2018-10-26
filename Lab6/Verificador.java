9811962e2d9af1033340cd9e31b40815f3590e53
AndyFernandes <andfernandes24@gmail.com>
2018-10-26 00:36:50 -0300

    Definicao das matrizes max e allocation

--- Lab6/Verificador.java
+++ Lab6/Verificador.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.stream.IntStream; 
 
 public class Verificador {
+	private static final String VIRGULA = ",";
 	int available[];
 	int[][] max;
 	int[][] allocation;
@@ -23,15 +24,33 @@ public class Verificador {
         }
 
         // calcular n e m  e atribuir a variaveis
+        // aparentemente n = processos.size() e m = processos[0].split() -> Ai (tamanho do vetor splitado - 1 )/2
+        int n = this.processos.size();
+        String[] aux = this.processos.get(0).split(",");
+        int m = (aux.length - 1)/2;
+        
         // instanciar os bixos
-        // max = new int[n][m];
+        max = new int[n][m];
+        available = new int[m];
+        allocation = new int[n][m];
+        need = new int[n][m];
+
         // quando terminar de ler aqui povoar o available, max, allocation, need
+        for(int i = 0; i < n; i++){
+        	aux = this.processos.get(i).split(",");
+        	for(int j = 0; j < m; j++){
+        		allocation[i][j] = Integer.parseInt(aux[j+1]);
+        		max[i][j] = Integer.parseInt(aux[j+4]);
+        	}
+        }
 	}
 
 	public void safety(){}
 
 	public void avoid(String pi){}
 	public void detection(){}
+
+	public static void main(String[] args) throws Exception{}
 
 
 }
